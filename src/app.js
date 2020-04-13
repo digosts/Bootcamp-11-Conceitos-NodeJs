f78b8c51979f04dafe92f0f1047dc80d53e047d5
digosts <rsantosdesenv@gmail.com>
2020-04-13 15:37:02 -0300

    Desenvolvimento do desafio.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,88 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+
+  try{
+    const repositorie = {
+      id:uuid(), 
+      title, 
+      url, 
+      techs, 
+      likes: 0
+    }
+
+    repositories.push(repositorie);
+  
+    return response.json(repositorie);
+  }catch(e){
+    return response.status(500).json({error:'Não foi possível realizar essa operação'});
+  }
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+  
+  try{
+    const repositorieIndex = repositories.findIndex(repo => repo.id === id);
+
+    if(repositorieIndex < 0){
+      return response.status(400).json({error:'Id não encontrado!'});
+    }
+
+    const repositorie = {
+      title,
+      url,
+      techs
+    }
+
+    repositories[repositorieIndex] = repositorie;
+
+    return response.json(repositorie);
+  }catch(e){
+    return response.status(500).json({error:'Não foi possível realizar essa operação'});
+  }
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  try{
+    const repositorieIndex = repositories.findIndex(repo => repo.id === id);
+
+    if(repositorieIndex < 0){
+      return response.status(400).json({error:'Id não encontrado!'});
+    }
+
+    repositories.splice(repositorieIndex, 1);
+
+    return response.status(204).send();
+  }catch(e){
+    return response.status(500).json({error:'Não foi possível realizar essa operação'});
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  
+  try{
+    const repositorieIndex = repositories.findIndex(repo => repo.id === id);
+
+    if(repositorieIndex < 0){
+      return response.status(400).json({error:'Id não encontrado!'});
+    }
+
+    repositories[repositorieIndex].likes++;
+
+    return response.json(repositories[repositorieIndex]);
+  }catch(e){
+    return response.status(500).json({error:'Não foi possível realizar essa operação'});
+  }
 });
 
 module.exports = app;
